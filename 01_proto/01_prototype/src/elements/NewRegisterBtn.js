3de30532c93358201950926bef8000592a1332f0
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-05-23 14:19:02 +0900

    refs #2 add new register button

---
+++ 01_proto/01_prototype/src/elements/NewRegisterBtn.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, View, Button} from 'react-native';
+
+class NewRegisterBtn extends React.Component {
+  onPress(){
+    //
+  }
+  render() {
+    return (
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={this.onPress}
+          title="新規登録"
+          color="#fff"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    height: 40,
+    width: 300,
+    padding: 5,
+    backgroundColor: '#e5697a',
+    margin: 3,
+    borderRadius: 5,
+  },
+});
+
+export default NewRegisterBtn;
--- 01_proto/01_prototype/src/screens/Login.js
+++ 01_proto/01_prototype/src/screens/Login.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+// import ons from 'onsenui';
+// import { Page, Toolbar, Button } from 'react-onsenui';
 
 import Logo from '../elements/Logo';
+import NewRegisterBtn from '../elements/NewRegisterBtn';
+
+// const ons = require('onsenui');
+// const Ons = require('react-onsenui');
 
 class Login extends React.Component {
   render() {
     return (
       <View>
         <Logo />
+        <NewRegisterBtn />
       </View>
     );
   }
 }
 
 export default Login;
+
+// <Page>
+//   <Toolbar>
+//     <div className="center">My title</div>
+//   </Toolbar>
+//   <Button onClick={this.handleClick}>Click me!</Button>
+// </Page>
